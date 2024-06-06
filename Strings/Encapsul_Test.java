5028e78fb5c018a8e4c93c0b4fdbd19d50d6ddd7
Ashish <ashish550chaubey@gmail.com>
2024-06-06 23:51:08 +0530

    06-06-2024

--- Strings/Encapsul_Test.java
+++ Strings/Encapsul_Test.java
@@ -1,26 +1,55 @@
+// class Student {
+// private int age;
+// private String name;
+
+// Student(int age, String name) {
+// this.name = name;
+// this.age = age;
+// }
+
+// public int getAge() {
+// return age;
+// }
+
+// public String getName() {
+// return name;
+// }
+
+// public void show() {
+// System.out.println(name + " " + age);
+// }
+// }
+
+// public class Encapsul_Test {
+// public static void main(String[] args) {
+// Student obj = new Student(23, "Ashish");
+// Student obj1 = new Student(20, "Prabhat");
+// System.out.println(obj1);
+// // obj.age = 23;
+// // obj.name = "Ashish";
+// // obj.setAge(21);
+// // obj.setName("Ashish");
+// // obj1.setName("Prabhat");
+// // obj1.setAge(19);
+// // System.out.println(obj.name);
+// // System.out.println(obj.age);
+// }
+// }
+// *******************************************************2nd_Question*************************************/
 class Student {
-    private int age;
+    private int a;
     private String name;
 
-    public void setData() {
-        age = 23;
-        name = "Ashish";
-    }
+    void disp(int a, String name) {
+        System.out.println(a);
+        System.out.println(name);
 
-    public void show() {
-        System.out.println(name + " " + age);
     }
 }
 
 public class Encapsul_Test {
     public static void main(String[] args) {
-        Student obj = new Student();
-        // obj.age = 23;
-        // obj.name = "Ashish";
-        obj.setData();
-        obj.show();
-        // System.out.println(obj.name);
-        // System.out.println(obj.age);
-
+        Student obj1 = new Student();
+        obj1.disp(19, "Ashish");
     }
 }
